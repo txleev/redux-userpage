e3eb402e66a84ee7391f282589c079c2f506f53e
N T <tuleevnursultan@gmail.com>
2025-04-10 23:26:56 +0600

    initt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className="bg-white shadow-md py-4 px-8 sticky top-0 z-50">
       <div className="flex justify-between items-center">
-        <Link to="/users" className="text-xl font-bold text-blue-600">
-          🧑‍💻 UserList
+        <Link to="/users" className="text-xl font-bold text-red-600">
+          List of users
         </Link>
       </div>
     </header>
